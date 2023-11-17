d22d4fde3a8883e5b473e4e1522092077e4e5f42
Marius <m.gzegozevskis@gmail.com>
2023-11-17 23:45:44 +0200

    Starter Project

---
+++ src/main/java/eif/viko/lt/bd/talpykla/eifbox/EifboxApplication.java
@@ -0,0 +1,13 @@
+package eif.viko.lt.bd.talpykla.eifbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EifboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EifboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eif/viko/lt/bd/talpykla/eifbox/EifboxApplicationTests.java
@@ -0,0 +1,13 @@
+package eif.viko.lt.bd.talpykla.eifbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EifboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
