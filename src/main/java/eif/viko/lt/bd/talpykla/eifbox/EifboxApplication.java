a35714adb09e65b26092ce07c61e22f97f922eb0
Marius <m.gzegozevskis@gmail.com>
2023-11-20 01:43:17 +0200

    removed amazon

--- src/main/java/eif/viko/lt/bd/talpykla/eifbox/EifboxApplication.java
+++ src/main/java/eif/viko/lt/bd/talpykla/eifbox/EifboxApplication.java
@@ -4,7 +4,6 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
